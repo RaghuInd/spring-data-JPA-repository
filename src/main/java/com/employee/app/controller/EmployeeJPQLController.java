450b6645f2f579f55036ef27d831bd6b884f5e66
Raghu <rdgowda79@gmail.com>
2022-11-29 17:24:52 +0530

    Added more JPA Custom queries

--- src/main/java/com/employee/app/controller/EmployeeJPQLController.java
+++ src/main/java/com/employee/app/controller/EmployeeJPQLController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.employee.app.dao.EmployeeRepository;
 import com.employee.app.entity.Employee;
+
+import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("jpql")
@@ -25,5 +28,19 @@ public class EmployeeJPQLController {
 	@GetMapping("employee/{id}")
 	public Employee getEmployeeByid(@PathVariable("id") Integer id){
 		return this.empRepo.findEmployeeById(id);
+	}
+	
+	@PatchMapping("employee/{id}/{name}")
+	@Transactional
+	public String updateEmployeeNameByid(@PathVariable("id") Integer id,@PathVariable("name") String name){
+		 this.empRepo.updateEmployeeNameById(id,name);
+		 return "Employee name updated.";
+	}
+	
+	@GetMapping("employee/{from}/{to}")
+	@Transactional
+	public List<Employee> getEmployeeBetweenSalry(@PathVariable Double from,@PathVariable("to") Double toSal){
+		 
+		 return this.empRepo.findBySalaryBetween(from,toSal);
 	}
 }
